9295f9040c5bc70ca76e241de1787a8d12b908d0
BastelKlug <44060099+BastelKlug@users.noreply.github.com>
2022-01-08 22:08:24 +0100

    Filamentlängen Berechner hinzugefügt

---
+++ calculator/extrusion/extrusion.js
@@ -0,0 +1,12 @@
+document.getElementById("berechnen").onclick = function(){
+    var laengeVorher = document.getElementById("laengeVorher").value;
+    var laengeAktuell = document.getElementById("laengeAktuell").value;
+
+
+    laengeVorher = parseFloat(laengeVorher.toString().replace(",","."));
+    laengeAktuell = parseFloat(laengeAktuell.toString().replace(",","."));
+
+
+    var ergebnis = (laengeVorher-laengeAktuell).toFixed(2);
+    document.getElementById("ergebnis").innerHTML = ergebnis;
+}
