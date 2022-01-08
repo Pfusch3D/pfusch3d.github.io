4509513a56f157e78df286b596cf322f9b403982
BastelKlug <44060099+BastelKlug@users.noreply.github.com>
2022-01-08 21:49:09 +0100

    Add Calculators
    Add calculators without css design! Sorry!

---
+++ calculator/rotationdistance/rotationdistance.js
@@ -0,0 +1,12 @@
+document.getElementById("berechnen").onclick = function(){
+    var stepsAktuell = document.getElementById("stepsAktuell").value;
+    var sollLaenge = document.getElementById("sollLaenge").value;
+    var istLaenge = document.getElementById("istLaenge").value;
+
+    stepsAktuell = parseFloat(stepsAktuell.toString().replace(",","."));
+    sollLaenge = parseFloat(sollLaenge.toString().replace(",","."));
+    istLaenge = parseFloat(istLaenge.toString().replace(",","."));
+
+    var ergebnis = (stepsAktuell*istLaenge/sollLaenge).toFixed(3);
+    document.getElementById("ergebnis").innerHTML = ergebnis;
+}
